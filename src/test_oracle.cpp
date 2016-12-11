89d0d28e368d94652e630897ba3bd911b7f6a18d
Bill Hass <billhass@umich.edu>
2016-12-11 09:17:06 -0800

    Modularized some of the padding oracle code

--- src/test_oracle.cpp
+++ src/test_oracle.cpp
@@ -27,6 +27,16 @@
 #define FALSE 0
 #endif
 
+#define BLOCKSIZE 16
+#define BUFSIZE 4096 
+
+int find_padding(unsigned char *, std::vector<unsigned char>);
+std::vector<unsigned char> makeV1(int);
+std::vector<unsigned char> makeV3(int, std::vector<unsigned char>, int);
+
+unsigned char ckey[] = "1122334455667788";
+unsigned char ivec[] = "llmmnnooppqqrrss";
+
 void printBytes(unsigned char* buf, int len){
   for(int i = 0; i < len; i++ ) {
     putc( isprint(buf[i]) ? buf[i] : '.' , stdout );
@@ -39,8 +49,6 @@ void printBytes(unsigned char* buf, int len){
  */
 void en_de_crypt(int should_encrypt, FILE *ifp, FILE *ofp, unsigned char *ckey, unsigned char *ivec, std::string mode) {
 
-  //Arbitrary BUFSIZE for chunking the target file.
-  const unsigned BUFSIZE=4096;
   unsigned char *read_buf = (unsigned char*) malloc(BUFSIZE);
   unsigned char *cipher_buf;
   unsigned blocksize;
@@ -90,117 +98,63 @@ void en_de_crypt(int should_encrypt, FILE *ifp, FILE *ofp, unsigned char *ckey, 
   EVP_CIPHER_CTX_free(ctx);
 }
 
-int padding_oracle(std::vector<unsigned char> cipher, unsigned char *ckey, unsigned char *ivec){
+int padding_oracle(unsigned char* ivec, std::vector<unsigned char> cipher){
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    const unsigned BUFSIZE=4096;
     unsigned char *read_buf = (unsigned char*) malloc(BUFSIZE);
     int out_len, err;
-    unsigned blocksize;
-    unsigned char *cipher_buf = (unsigned char *) malloc(BUFSIZE + blocksize);
-    //Initialize the cipher envelope as 256-bit CBC with ckey, iv, enc/dec mode
-    //cipher[31] = 0x2;
+    unsigned char *cipher_buf = (unsigned char *) malloc(BUFSIZE + BLOCKSIZE);
     EVP_CipherInit(ctx, EVP_aes_128_cbc(), ckey, ivec, 0);
-    blocksize = EVP_CIPHER_CTX_block_size(ctx);
-
-
-    //Update cipher (Uses CBC mode EVP API)
-    
-    // std::cout << "cipher data: " << cipher.data() << std::endl;
-    // std::cout << "Size: " << cipher.size() << std::endl;
-    //unsigned char s = 0x15;
-    //cipher[44] = s;
-    // printf("%x\n", cipher[30]);
-    // printf("%x\n", cipher[31]);
-    //cipher[30] = 0x02;
-    //cipher[31] = 0x01;
-    //std::cout << std::endl;
-    //printf("%x\n", cipher[15]);
-    //cipher[15] = cipher[15]^0x71^0x10;
-    //printf("%x\n", cipher[15]);
-    
-    //printf("%x\n", cipher[30]);
-    //printf("%x\n", cipher[31]);
     err = EVP_CipherUpdate(ctx, cipher_buf, &out_len, cipher.data(), cipher.size());
-    // std::cout << "Output length: " << out_len << " err: " << err << std::endl;
     err = EVP_CipherFinal_ex(ctx, cipher_buf, &out_len);
-    // std::cout << "Output length: " << out_len << " err: " << err << std::endl;
-    // std::cout << "Cipher_buf " << cipher_buf << std::endl;
     EVP_CIPHER_CTX_free(ctx);
     return err;
-
 }
 
-void padding_oracle_attack(int should_encrypt, std::string filename, FILE *ofp, unsigned char *ckey, unsigned char *ivec){
+void padding_oracle_attack(int should_encrypt, std::string filename, FILE *ofp){
   typedef std::istream_iterator<unsigned char> istream_iterator;
   std::ifstream file(filename);
   std::vector<unsigned char> cipher;
   file >> std::noskipws;
   std::copy(istream_iterator(file), istream_iterator(), std::back_inserter(cipher));
-  int padding_start = 0;
-  const unsigned BUFSIZE=4096;
-  unsigned char temp;
+  unsigned char ivec[] = "llmmnnooppqqrrss";
 
   //Scan through cipher to see if changing a byte causes padding error
   std::cout << "CipherSize: " << cipher.size() << std::endl;
 
-  for(unsigned i = 0; i < 16; i++){
-    temp = cipher[i];
-    cipher[i]++;
-    if(padding_oracle(cipher, ckey, ivec) == 0){
-        //Padding error detected
-        padding_start = i + 1;
-        cipher[i] = temp;
-        break;
-    }
-    cipher[i] = temp;
-  }
+  int pad_start = find_padding(ivec, cipher);
+  int b = BLOCKSIZE - pad_start;
 
-  int b = 16 - padding_start;
+  //our first guess is size of block minus pad
+  std::vector<unsigned char> plaintext(BLOCKSIZE - b); 
 
-  std::vector<unsigned char> plaintext(cipher.size() - b);
   for(unsigned k = 16; k > 0; k--){
     std::vector<unsigned char> tempCipher = cipher;
-    std::cout << "Padding starts at " << padding_start << " b = "<< b << std::endl;
-    std::vector<unsigned char> v1(16), v2(16), v3(16);
+    std::cout << "k= "<<k<<" Padding starts at " << pad_start << " b = "<< b << std::endl;
+    std::vector<unsigned char> v1(BLOCKSIZE), v3(BLOCKSIZE);
 
-    for(unsigned i = 0; i < padding_start; ++i){ //Fill 0's up to padding
-      v1.at(i) = (unsigned char)0x00;
-    }
-    for(unsigned i = padding_start; i < 16; ++i){ //Fill b padding bytes
-      v1.at(i) = (unsigned char)b;
-    }
-
-    int index = padding_start - 1; //i index (what we are searching for)
-    for(unsigned i = 0; i < index; ++i){ //Pre-fill with 0's
-      v2.at(i) = (unsigned char)(0x00);
-    }
-    for(unsigned i = padding_start; i < 16; ++i){
-      v2.at(i) = (unsigned char)(b+1); //Post-fill with b+1 padding bytes
-    }      
+    v1 = makeV1(b);
 
     std::cout << "v1: ";
     printBytes(v1.data(), v1.size());
-    std::cout << std::endl << "v2: ";
+    std::cout << std::endl;/* << "v2: ";
     printBytes(v2.data(), v2.size());
-    std::cout << std::endl;
+    std::cout << std::endl;*/
 
     //Begin looping to find value of i (which tells us plaintext byte at index)
     for(unsigned i = 0; i < 256; ++i){
+      v3 = makeV3(b, v1, i);
 
-      v2.at(index) = (unsigned char)(i); //Assign i to v2
-      for(unsigned j = 0; j < 16; j++){ 
-        v3.at(j) = v1.at(j)^v2.at(j);//Compute Delta = v3
-      }
-      for(unsigned j = 0; j < 16; ++j){
+      for(unsigned j = 0; j < BLOCKSIZE; ++j){
         cipher.at(j) = v3.at(j)^cipher.at(j);
       }
+
       unsigned char B;
-      if(padding_oracle(cipher, ckey, ivec)){
+      if(padding_oracle(ivec, cipher)){
         B = (unsigned char)(b+1)^(unsigned char)i;
         std::cout << "B: " << B;
         std::cout << std::endl;
         plaintext.insert(plaintext.begin(),B);
-        padding_start--;
+        pad_start--;
         b++;
         break;
       }
@@ -208,7 +162,7 @@ void padding_oracle_attack(int should_encrypt, std::string filename, FILE *ofp, 
         cipher = tempCipher;
       }
     }
-    if(padding_start == 0){
+    if(pad_start == 0){
       std::cout << "Plaintext: ";
       printBytes(plaintext.data(), plaintext.size());
       std::cout<<std::endl;
@@ -217,12 +171,55 @@ void padding_oracle_attack(int should_encrypt, std::string filename, FILE *ofp, 
   }
 }
 
+
+// Returns the index where the padding starts in the ciphertext
+int find_padding(unsigned char * ivec, std::vector<unsigned char> cipher){
+  unsigned char temp;
+  for(int i = cipher.size()-(2*BLOCKSIZE); i < cipher.size()-BLOCKSIZE; i++){
+    temp = cipher.at(i);
+    cipher.at(i)++; //Change cipher block at i
+    //Now use padding oracle with modified cipher_block
+    if(padding_oracle(ivec, cipher) == 0){
+        //Padding error detected
+        return  i; //0, 1, 2, etc.
+    }
+    cipher.at(i) = temp;
+  }
+}
+
+std::vector<unsigned char> makeV1(int b){
+  std::vector<unsigned char> v1(BLOCKSIZE);
+  int pad_start = -1*(b - BLOCKSIZE);
+    for(int i = 0; i < pad_start; ++i){ //Fill 0's up to padding
+      v1.at(i) = (unsigned char)0x00;
+    }
+    for(int i = pad_start; i < BLOCKSIZE; ++i){ //Fill b padding bytes
+      v1.at(i) = (unsigned char)b;
+    }
+  return v1;
+}
+
+
+std::vector<unsigned char> makeV3(int b, std::vector<unsigned char>v1, int guess){
+    int pad_start = -1*(b - BLOCKSIZE);
+    std::vector<unsigned char> v2(BLOCKSIZE), v3(BLOCKSIZE);
+    for(unsigned i = 0; i < pad_start - 1; ++i){ //Pre-fill with 0's
+      v2.at(i) = (unsigned char)(0x00);
+    }
+    v2.at(pad_start-1) = (unsigned char) guess; //Add the guess
+    for(unsigned i = pad_start; i < BLOCKSIZE; ++i){
+      v2.at(i) = (unsigned char) (b+1); //Post-fill with b+1 padding bytes
+    }      
+    for(unsigned j = 0; j < BLOCKSIZE; j++){ 
+      v3.at(j) = v1.at(j)^v2.at(j);//Compute V3
+    }
+    return v3;
+}
+
 int main(int argc, char *argv[])
 { 
   std::cout << "Hello Crypto." << std::endl;	
 
-  unsigned char ckey[] = "thiskeyisverybad";
-  unsigned char ivec[] = "dontusethisinput";
   FILE *fIN, *fOUT;
 
   // First encrypt the file
@@ -238,7 +235,7 @@ int main(int argc, char *argv[])
   //Decrypt file now
   fIN = fopen("origcipher.txt", "rb"); //File to be read; cipher text
   fOUT = fopen("decrypted.txt", "wb"); //File to be written; plain text
-  padding_oracle_attack(0, "origcipher.txt", fOUT, ckey, ivec);
+  padding_oracle_attack(0, "origcipher.txt", fOUT);
 
   std::cout << "Decrypting contents of ciphertext.txt:" << std::endl;	
   en_de_crypt(FALSE, fIN, fOUT, ckey, ivec, "CBC");
